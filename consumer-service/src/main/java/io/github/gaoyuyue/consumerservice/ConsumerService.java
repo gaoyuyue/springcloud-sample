f46a5ecabf2e545690328e7249fe34ce813af6fd
高语越 <gaoyuyue@outlook.com>
2018-11-13 11:18:57 +0800

    add extra hystrix-dashboard service

--- consumer-service/src/main/java/io/github/gaoyuyue/consumerservice/ConsumerService.java
+++ consumer-service/src/main/java/io/github/gaoyuyue/consumerservice/ConsumerService.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@EnableHystrixDashboard
 @EnableCircuitBreaker
 public class ConsumerService {
     public static void main(String[] args) {
---
+++ hystrix-dashboard/src/main/java/io/github/gaoyuyue/hystrixdashboard/HystrixDashboard.java
@@ -0,0 +1,17 @@
+package io.github.gaoyuyue.hystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableTurbine
+@EnableHystrixDashboard
+public class HystrixDashboard {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboard.class, args);
+    }
+}
