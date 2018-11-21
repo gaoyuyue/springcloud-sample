89baabc76c112985239ff8841aba57fd397e68ed
高语越 <gaoyuyue@outlook.com>
2018-11-21 21:39:26 +0800

    为api-gateway和producer添加认证服务

--- producer-service/src/main/java/io/github/gaoyuyue/producerservice/ProducerService.java
+++ producer-service/src/main/java/io/github/gaoyuyue/producerservice/ProducerService.java
@@ -3,9 +3,11 @@ package io.github.gaoyuyue.producerservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ProducerService {
     public static void main(String[] args) {
         SpringApplication.run(ProducerService.class, args);
